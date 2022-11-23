7f916dc18d5e40a65e82c22fb62efb4186d7272a
patelshivani3 <shivpatel3001@gmail.com>
2022-11-23 10:35:30 +0530

    Added:Review2

---
+++ leapyear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+read -p "Enter the year: " year;
+
+if [ $((year%4)) -eq 0 ]
+then
+	echo "$year is a leap year"
+
+elif [ $((year%400)) -eq 0 ]
+then
+	echo "$year is a leap year"
+elif [ $((year%100)) -eq 0 ]
+then
+	echo "$year is not leap year"
+else
+	echo "$year is not leap year"
+fi
